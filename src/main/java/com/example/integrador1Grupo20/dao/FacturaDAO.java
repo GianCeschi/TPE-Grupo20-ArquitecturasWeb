928cf0ac6ff1c4ac050cb1a41903ab474d111ae3
Gianluca Ceschinelli <ceschinelligianluca@gmail.com>
2024-09-11 15:59:26 -0300

    Se termina facturaDAO

--- src/main/java/com/example/integrador1Grupo20/dao/FacturaDAO.java
+++ src/main/java/com/example/integrador1Grupo20/dao/FacturaDAO.java
@@ -2,7 +2,10 @@ package com.example.integrador1Grupo20.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.example.integrador1Grupo20.entities.Factura;
 
@@ -84,5 +87,76 @@ public class FacturaDAO {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public Factura find(Integer pk) {
+		String query = "SELECT f.idCliente " +
+				"FROM Factura f " +
+				"WHERE f.idFactura = ?";
+		Factura facturaById = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			ps = conn.prepareStatement(query);
+			ps.setInt(1, pk); // Establecer el parámetro en la consulta SQL
+			rs = ps.executeQuery();
+			if (rs.next()) { // Verificar si hay resultados
+				int idCliente = rs.getInt("idCliente");
+
+				// Crear una nueva instancia de Persona con los datos recuperados de la consulta
+				facturaById = new Factura(pk, idCliente);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+				conn.commit();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return facturaById;
+	}
+
+
+	public List<Factura> selectListFacturas() {
+		String query = "SELECT * " +
+				"FROM Factura ";
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<Factura> listado = null;
+		try {
+			ps = conn.prepareStatement(query);
+			rs = ps.executeQuery();
+			// Crear una nueva instancia de Persona con los datos recuperados de la consulta
+			listado = new ArrayList<Factura>();
+			while (rs.next()) { // Verificar si hay resultados
+				int idFactura = rs.getInt("idFactura");
+				int idCliente = rs.getInt("idCliente");
+				Factura factura = new Factura(idFactura, idCliente);
+				listado.add(factura);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+				conn.commit();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return listado;
 	}
 }
